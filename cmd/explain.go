2abff6daae3ee4f0a66c6385ccd851db3f36b867
Matthias Alt <mattagohni@gmail.com>
2023-04-29 17:26:21 +0200

    refactor(kbuddy): for better testability

--- cmd/explain.go
+++ cmd/explain.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	responseFormat = `
+	ResponseFormat = `
 {
 	keyword: "The keyword to explain",
 	explanation: "the up to 1000 word explanation of the given keyword",
@@ -35,67 +35,15 @@ const (
 )
 
 // explainCmd represents the explain command
-var explainCmd = &cobra.Command{
-	Use:   "explain",
-	Short: "Will explain given topic related to kubernetes using ChatGPT",
-	Long:  `given keyword e.g. (Deployment) will be explained using ChatGPT`,
-	Run: func(cmd *cobra.Command, args []string) {
-		color.Set(color.FgHiBlue)
-		var messages []goopenai.Message
-		apiKey := os.Getenv("OPEN_AI_API_KEY")
-		organization := os.Getenv("OPEN_AI_API_ORG")
-
-		client := goopenai.NewClient(apiKey, organization)
-
-		var givenSearchTerm = ""
-		if len(args) >= 1 && args[0] != "" {
-			givenSearchTerm = args[0]
-		}
-		message := goopenai.Message{
-			Role: "user",
-			Content: fmt.Sprintf(
-				"explain %s in context of kubernetes. in your response make a new line every 80"+
-					" charecters. also structure your response in a json with the following format "+responseFormat, givenSearchTerm),
-		}
-		messages = append(messages, message)
-		r := goopenai.CreateCompletionsRequest{
-			Model:       "gpt-3.5-turbo",
-			Messages:    messages,
-			Temperature: 0.2,
-		}
-
-		completions, err := client.CreateCompletions(context.Background(), r)
-		check(err)
-
-		var explainResponse ExplainResponse
-		if err := json.Unmarshal([]byte(completions.Choices[0].Message.Content), &explainResponse); err != nil {
-			panic(err)
-		}
-
-		keyword := color.HiMagentaString(explainResponse.Keyword)
-		explanation := color.HiWhiteString(explainResponse.Explanation)
-		var furtherReadings []string
-		disclaimer := color.HiYellowString(explainResponse.Disclaimer)
-
-		for _, reading := range explainResponse.FurtherReading {
-			furtherReadings = append(furtherReadings, color.WhiteString(reading.Keyword+"\n"+reading.Description+"\n"+reading.Link))
-		}
-
-		yamlExample := explainResponse.ExampleYaml
-		jsonExample := explainResponse.ExampleJson
-
-		fmt.Println(keyword + "\n")
-		fmt.Println(explanation + "\n")
-		fmt.Println(disclaimer + "\n")
-		fmt.Println(yamlExample)
-		fmt.Println(jsonExample)
-		for _, reading := range furtherReadings {
-			fmt.Println(reading + "\n")
-		}
-	},
-}
+var explainCmd = &cobra.Command{}
 
 func init() {
+	apiKey := os.Getenv("OPEN_AI_API_KEY")
+	organization := os.Getenv("OPEN_AI_API_ORG")
+
+	client := NewMyOpenAiClient(apiKey, organization)
+
+	explainCmd := NewExplainCommand(client.client)
 	rootCmd.AddCommand(explainCmd)
 
 	// Here you will define your flags and configuration settings.
@@ -107,6 +55,70 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// explainCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func NewExplainCommand(client OpenAiClient) *cobra.Command {
+	var explainCmd = &cobra.Command{
+		Use:   "explain",
+		Short: "Will explain given topic related to kubernetes using ChatGPT",
+		Long:  `given keyword e.g. (Deployment) will be explained using ChatGPT`,
+		Run: func(cmd *cobra.Command, args []string) {
+			color.Set(color.FgHiBlue)
+			var messages []goopenai.Message
+
+			var givenSearchTerm = ""
+			if len(args) >= 1 && args[0] != "" {
+				givenSearchTerm = args[0]
+			}
+
+			message := goopenai.Message{
+				Role: "user",
+				Content: fmt.Sprintf(
+					"explain %s in context of kubernetes. in your response make a new line every 80"+
+						" charecters. also structure your response in a json with the following format "+ResponseFormat, givenSearchTerm),
+			}
+			messages = append(messages, message)
+			r := goopenai.CreateCompletionsRequest{
+				Model:       "gpt-3.5-turbo",
+				Messages:    messages,
+				Temperature: 0.2,
+			}
+
+			completions, err := client.CreateCompletions(context.Background(), r)
+			check(err)
+
+			var explainResponse ExplainResponse
+			if err := json.Unmarshal([]byte(completions.Choices[0].Message.Content), &explainResponse); err != nil {
+				panic(err)
+			}
+
+			keyword := color.HiMagentaString(explainResponse.Keyword)
+			explanation := color.HiWhiteString(explainResponse.Explanation)
+			var furtherReadings []string
+			disclaimer := color.HiYellowString(explainResponse.Disclaimer)
+
+			for _, reading := range explainResponse.FurtherReading {
+				furtherReadings = append(furtherReadings, color.WhiteString(reading.Keyword+"\n"+reading.Description+"\n"+reading.Link))
+			}
+
+			yamlExample := explainResponse.ExampleYaml
+			jsonExample := explainResponse.ExampleJson
+
+			fmt.Println(keyword + "\n")
+			fmt.Println(explanation + "\n")
+			fmt.Println(disclaimer + "\n")
+			fmt.Println(yamlExample)
+			fmt.Println(jsonExample)
+			for _, reading := range furtherReadings {
+				fmt.Println(reading + "\n")
+			}
+		},
+	}
+
+	// add any flags or arguments that the command needs
+	// ...
+
+	return explainCmd
 }
 
 func check(e error) {
--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/mattagohni/kbuddy
 go 1.20
 
 require (
+	github.com/fatih/color v1.15.0
 	github.com/franciscoescher/goopenai v0.4.0
 	github.com/spf13/cobra v1.7.0
+	github.com/stretchr/testify v1.8.1
 	k8s.io/api v0.27.1
+	k8s.io/apimachinery v0.27.1
 	k8s.io/client-go v0.27.1
 )
 
 require (
-	github.com/fatih/color v1.15.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
@@ -21,13 +24,15 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/apimachinery v0.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
