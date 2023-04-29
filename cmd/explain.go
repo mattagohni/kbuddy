313091dbfdcf1f3afc59145c7c510a80b2302c38
Matthias Alt <mattagohni@gmail.com>
2023-04-29 12:58:57 +0200

    feat(kbuddy): use ExplainResponse to standardize the response of chatgpt and parse the response into a known data structure

--- cmd/explain.go
+++ cmd/explain.go
@@ -1,15 +1,37 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Matthias Alt <mattagohni@gmail.com>
 */
 package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/franciscoescher/goopenai"
+	. "github.com/mattagohni/kbuddy/internal/response"
 	"github.com/spf13/cobra"
 	"log"
 	"os"
+)
+
+const (
+	responseFormat = `
+{
+	keyword: "The keyword to explain",
+	explanation: "the up to 1000 word explanation of the given keyword",
+	disclaimer: "disclaimer about correctness with with a link to the kubernetes docs",
+	exampleYaml: "the yaml definition of the resource of the given keyword if the keyword is a kubernetes resource, empty otherwise",
+	exampleJson: "the json definition of the resource of the given keyword if the keyword is a kubernetes resource, empty otherwise",
+	furtherReading: [
+	    {
+			keyword: the keyword
+			description: short description of the topic
+			link: hyperlink to the kubernetes docs if possible
+		}
+	]
+}
+`
 )
 
 // explainCmd represents the explain command
@@ -18,6 +40,7 @@ var explainCmd = &cobra.Command{
 	Short: "Will explain given topic related to kubernetes using ChatGPT",
 	Long:  `given keyword e.g. (Deployment) will be explained using ChatGPT`,
 	Run: func(cmd *cobra.Command, args []string) {
+		color.Set(color.FgHiBlue)
 		var messages []goopenai.Message
 		apiKey := os.Getenv("OPEN_AI_API_KEY")
 		organization := os.Getenv("OPEN_AI_API_ORG")
@@ -28,13 +51,11 @@ var explainCmd = &cobra.Command{
 		if len(args) >= 1 && args[0] != "" {
 			givenSearchTerm = args[0]
 		}
-		fmt.Printf("given search term for explanation %s\n", givenSearchTerm)
-		fmt.Printf("using API-Key %s\n", apiKey)
-		fmt.Printf("for Org %s\n", organization)
-
 		message := goopenai.Message{
-			Role:    "user",
-			Content: fmt.Sprintf("explain %s in context of kubernetes", givenSearchTerm),
+			Role: "user",
+			Content: fmt.Sprintf(
+				"explain %s in context of kubernetes. in your response make a new line every 80"+
+					" charecters. also structure your response in a json with the following format "+responseFormat, givenSearchTerm),
 		}
 		messages = append(messages, message)
 		r := goopenai.CreateCompletionsRequest{
@@ -46,7 +67,31 @@ var explainCmd = &cobra.Command{
 		completions, err := client.CreateCompletions(context.Background(), r)
 		check(err)
 
-		fmt.Println(completions)
+		var explainResponse ExplainResponse
+		if err := json.Unmarshal([]byte(completions.Choices[0].Message.Content), &explainResponse); err != nil {
+			panic(err)
+		}
+
+		keyword := color.HiMagentaString(explainResponse.Keyword)
+		explanation := color.HiWhiteString(explainResponse.Explanation)
+		var furtherReadings []string
+		disclaimer := color.HiYellowString(explainResponse.Disclaimer)
+
+		for _, reading := range explainResponse.FurtherReading {
+			furtherReadings = append(furtherReadings, color.WhiteString(reading.Keyword+"\n"+reading.Description+"\n"+reading.Link))
+		}
+
+		yamlExample := explainResponse.ExampleYaml
+		jsonExample := explainResponse.ExampleJson
+
+		fmt.Println(keyword + "\n")
+		fmt.Println(explanation + "\n")
+		fmt.Println(disclaimer + "\n")
+		fmt.Println(yamlExample)
+		fmt.Println(jsonExample)
+		for _, reading := range furtherReadings {
+			fmt.Println(reading + "\n")
+		}
 	},
 }
 
